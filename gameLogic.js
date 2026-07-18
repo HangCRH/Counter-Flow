7fa343dd0821edc52a589633a8e6f4bb832dc665
HangCRH <1317806770@qq.com>
2026-07-18 16:28:39 +0800

    开始游戏时生成玩家和AI

--- gameLogic.js
+++ gameLogic.js
@@ -1,30 +1,38 @@
-var aiPlayerCount = 0;      // 记录AI数量
-var gameData = new gameBoard()   // 记录地图信息（游戏信息）
-var isPlaying = false;  // 记录是否正在进行游戏
-
-class gameBoard {
+class gameObject {
     constructor() {
         this.players = [];
         this.cities = [];
     }
-    createPlayer(type, color) {
-
+    createPlayer(type, color = null) {
+        // 如果没有提供颜色则自动生成
+        if (!color) {
+            // 随机色相，饱和度固定 70%，明度固定 60%（避免太暗或太亮）
+            color = `hsl(${parseInt(Math.random() * 360)}, 70%, 60%)`;
+        }
+        this.players.push(new Player(type, color));
     }
 }
 
 class Player {
     constructor(type, color) {
-
+        this.type = type;
+        this.color = color;
+        this.ownCities = [];
     }
 }
 
+var gameBoard = new gameObject()   // 记录地图信息（游戏信息）
+var isPlaying = false;  // 记录是否正在进行游戏
+
 function init() {
     isPlaying = true;
-    loadAiPlayers();
+    loadPlayers(3);
+    console.log(gameBoard);
 }
 
-function loadAiPlayers(cnt = 0) {
-    for (let i = 0; i < cnt; i++) {
-
+function loadPlayers(aiCnt = 0) {
+    gameBoard.createPlayer("user")
+    for (let i = 0; i < aiCnt; i++) {
+        gameBoard.createPlayer("AI")
     }
 }
