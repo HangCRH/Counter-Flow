a001b5f688348d734b0751a460fcb9bee92287bd
HangCRH <1317806770@qq.com>
2026-07-18 15:44:02 +0800

    搭建游戏脚本骨架

--- gameLogic.js
+++ gameLogic.js
@@ -1,9 +1,24 @@
-var ctx = document.getElementsByTagName("canvas")[0].getContext("2d");
-var aiPlayerCount = 3;
+var aiPlayerCount = 0;      // 记录AI数量
+var gameData = new gameBoard()   // 记录地图信息（游戏信息）
 
-class aiPlayer {
-    constructor(color) {
-        this.color = color;
-        this.ownCity = [];
+class gameBoard {
+    constructor() {
+        this.players = [];
+        this.cities = [];
+    }
+    createPlayer(type, color) {
+
+    }
+}
+
+class Player {
+    constructor(type, color) {
+
+    }
+}
+
+function loadAiPlayers(cnt = 0) {
+    for (let i = 0; i < cnt; i++) {
+
     }
 }
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const WEBSITE_VERSION = "0.1.0";
+var ctx = document.getElementsByTagName("canvas")[0].getContext("2d");  // 画布对象
+var isPlaying = false;  // 记录是否正在进行游戏
 
 function init() {
-
+    isPlaying = true;
+    loadAiPlayers();
 }
